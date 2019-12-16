d8d9ffff6ff81f1710542187d36a2eadbf133680
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-15 16:54:26 -0800

    Created template with sidebar of page.php

---
+++ themes/gymfitnesswptheme/page-with-sidebar.php
@@ -0,0 +1,27 @@
+<?php
+/*
+* Template Name: Page with Sidebar
+*/
+get_header(); ?>
+    <main class="container page section no-sidebars">
+        <?php while(have_posts() ): the_post(); ?>
+            <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+                
+                <?php                 
+                    //Check if an image exists
+                    if( has_post_thumbnail() ):
+                        the_post_thumbnail('blog', array('class' => 'featured-image'));
+                    else:
+                        echo "<p> There is no image</p>";
+                    endif;
+                ?>
+                
+                <?php the_content(); ?>
+                
+        <?php endwhile; ?>
+
+        <aside>
+            <h2>Sidebar Here</h2>
+        </aside>
+    </main>
+<?php get_footer(); ?>
