99e75017fe0f319701b5659c3933f863d8f6ad99
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-15 18:36:43 -0800

    Add 60% content area and 40% sidebar area to pagee-with-sidebar.php and additional misc styles

--- themes/gymfitnesswptheme/page-with-sidebar.php
+++ themes/gymfitnesswptheme/page-with-sidebar.php
@@ -3,24 +3,26 @@
 * Template Name: Page with Sidebar
 */
 get_header(); ?>
-    <main class="container page section no-sidebars">
-        <?php while(have_posts() ): the_post(); ?>
-            <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-                
-                <?php                 
-                    //Check if an image exists
-                    if( has_post_thumbnail() ):
-                        the_post_thumbnail('blog', array('class' => 'featured-image'));
-                    else:
-                        echo "<p> There is no image</p>";
-                    endif;
-                ?>
-                
-                <?php the_content(); ?>
-                
-        <?php endwhile; ?>
+    <main class="container page section with-sidebar">
+        <div class="page-content">
+            <?php while(have_posts() ): the_post(); ?>
+                <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+                    
+                    <?php                 
+                        //Check if an image exists
+                        if( has_post_thumbnail() ):
+                            the_post_thumbnail('blog', array('class' => 'featured-image'));
+                        else:
+                            echo "<p> There is no image</p>";
+                        endif;
+                    ?>
+                    
+                    <?php the_content(); ?>
+                    
+            <?php endwhile; ?>
+        </div>
 
-        <aside>
+        <aside class="sidebar">
             <h2>Sidebar Here</h2>
         </aside>
     </main>
