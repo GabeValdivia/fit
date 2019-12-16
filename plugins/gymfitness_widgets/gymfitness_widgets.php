a50eb125e049ae06b3927b0daf6633a02ee15d53
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-16 15:19:25 -0800

    Add hyperlink to widget class titles, pull in content and thumbnails. Test /gymfitness_classes/ post

--- plugins/gymfitness_widgets/gymfitness_widgets.php
+++ plugins/gymfitness_widgets/gymfitness_widgets.php
@@ -49,7 +49,21 @@ class GymFitness_Classes_Widget extends WP_Widget {
             ?>
 
             <li class="sidebar-class">
-                <h3><?php the_title(); ?></h3>
+                <div class="sidebar-widget-image">
+                    <?php the_post_thumbnail('thumbnail'); ?>
+                </div>
+
+                <div class="class-content">
+                    <a href="<?php the_permalink(); ?>">
+                    <h3><?php the_title(); ?></h3>
+                    </a>
+
+                    <?php
+                        $start_time = get_field('start_time');
+                        $end_time = get_field('end_time');
+                    ?>
+                    <p><?php echo the_field('class_days') . " &mdash; " . $start_time . " to " . $end_time ?></p>
+                </div>
 
             </li>
 
