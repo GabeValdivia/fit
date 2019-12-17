ed04161ca4a85a1e245e0a973e5cad47bfc4c835
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-16 17:13:12 -0800

    Create code to add images from the WordPress gallery ins gallery.php template

--- themes/gymfitnesswptheme/gallery.php
+++ themes/gymfitnesswptheme/gallery.php
@@ -8,7 +8,26 @@
         <?php while(have_posts() ): the_post(); ?>
             <h1 class="text-center text-primary"><?php the_title(); ?></h1>
 
-            <?php the_content(); ?>        
+            <?php 
+                $gallery = get_post_gallery( get_the_ID(), false );
+
+                $gallery_images_ids = explode(',', $gallery['ids']);
+
+                echo "<pre>";
+                var_dump($gallery_images_ids);
+                echo "</pre>";
+            ?>   
+            
+            <ul class="gallery-images">
+                <?php
+                    foreach($gallery_images_ids as $id): 
+                        $image = wp_get_attachment_image_src($id, 'square');?>
+
+                        <img src="<?php echo $image[0] ?>" />
+                    
+                    <?php endforeach; ?>
+                ?>
+            </ul>
         <?php endwhile; ?>
     </main>
 
