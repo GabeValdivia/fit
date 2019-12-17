69494dc734c382b5e11b813c0ec6ccabd6fc607e
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-16 21:01:17 -0800

    Create 4 sample posts with featured image, tittle and copy

--- themes/gymfitnesswptheme/home.php
+++ themes/gymfitnesswptheme/home.php
@@ -2,7 +2,7 @@
 
 <main class="container page section">
     <?php while(have_posts() ): the_post(); ?>
-        echo "<h2><?php the_title(); ?></h2>"
+        <h2><?php the_title(); ?></h2>
     <?php endwhile; ?>
 </main>
 <?php get_footer(); ?>
