8cfec6d1a1dd83ea25db188899344a41f22d8ed4
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-16 21:37:33 -0800

    Add styles to date and author text on cards located on /blog/

--- themes/gymfitnesswptheme/home.php
+++ themes/gymfitnesswptheme/home.php
@@ -10,13 +10,14 @@
                         <h3><?php the_title(); ?></h3>
                     </a>
 
-                    <p class="meta"> <span>By: </span>
+                    <p class="meta"> <span class="text-primary">By: </span>
                         <a href="<?php echo get_author_posts_url( get_the_author_meta('ID')); ?>"> 
                             <?php echo get_the_author_meta('display_name'); ?>
                         </a>
                     </p>
 
                     <p class="date-published">
+                        <span class="text-primary">Date: </span>
                         <?php echo the_time( get_option('date_format') ); ?>
                     </p>
 
