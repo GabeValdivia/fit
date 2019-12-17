788841009e2a6a480cc2744402f9d58ef885b4f9
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-16 19:44:33 -0800

    Create blog template as required 'home.php' fiile and add the WP Loop

---
+++ themes/gymfitnesswptheme/home.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main class="container page section">
+    <?php while(have_posts() ): the_post(); ?>
+        echo "<h2><?php the_title(); ?></h2>"
+    <?php endwhile; ?>
+</main>
+<?php get_footer(); ?>
