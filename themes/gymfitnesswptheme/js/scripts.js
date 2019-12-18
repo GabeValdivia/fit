a56640fcd0c686365955b56e358742a7cb62df5a
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-17 23:44:20 -0800

    Add JavaScript for window.onscroll to get postionand add .fixed-top class, write CSS to add darker background to fixed-header and reduce size of logo. Also, make responsive margin/padding adjustments to nav so that it doesn't break on breakpoints just before 768px

--- themes/gymfitnesswptheme/js/scripts.js
+++ themes/gymfitnesswptheme/js/scripts.js
@@ -12,4 +12,27 @@ jQuery(document).ready(function($){
         ariaLive: true
     });
 
+    // When page is ready add fixed menu if position is > than 300px
+    const headerScroll = document.querySelector('.navigation-bar');
+    const rect = headerScroll.getBoundingClientRect();
+    const topDistance = Math.abs(rect.top);
+    fixedMenu(topDistance);
 });
+
+window.onscroll = () => {
+    const scroll = window.scrollY;
+    
+    fixedMenu(scroll);
+}
+
+// Adds a fixed menu on top
+function fixedMenu(scroll) {
+    const headerScroll = document.querySelector('.navigation-bar');
+
+    // In case the scroll is greater than 300 add some classes
+    if(scroll > 300) {
+        headerScroll.classList.add('fixed-top');
+    } else {
+        headerScroll.classList.remove('fixed-top');
+    }
+}
