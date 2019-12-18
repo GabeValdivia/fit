fc726ed2363141bcf6162d6b1ba891ccef90e8b4
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-17 23:13:24 -0800

    Refactor code for blog section, copy lul>loop from  home.php into the following: category.php, author.php, front-page.php. Test all templates to make sure all works

--- themes/gymfitnesswptheme/front-page.php
+++ themes/gymfitnesswptheme/front-page.php
@@ -95,6 +95,25 @@
             </ul>
         </div>
     </section>
+    <!-- Blog Section -->
+    <section class="blog container section">
+        <h2 class="text-center">Our Blog</h2>
+        <p class="text-center">Read our expert blog posts to achieve your goals.</p>
+        
+        <ul class="blog-entries">
+            <?php 
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => 4
+                );
+                $blog = new WP_Query($args);
+                while($blog->have_posts()): $blog->the_post() ?>
+                    <?php get_template_part('template-parts/blog', 'loop') ?>
+                <?php endwhile; wp_reset_postdata(); ?>
+            
+            ?>
+        </ul>
+    </section>
 
 <?php endwhile; ?>
 
