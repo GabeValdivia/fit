c3648a14f0eac01c50c112e2ffd5e43b15051267
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-17 17:53:19 -0800

    Add 'Areas' section in ACF, aadd images and text via homepage in WordPress, add code to pull in area_image & area_name in header-front.php page template

--- themes/gymfitnesswptheme/front-page.php
+++ themes/gymfitnesswptheme/front-page.php
@@ -7,6 +7,19 @@
         <p><?php the_field('welcome_text'); ?></p>
     </section>
 
+    <section class="section-areas">
+        <ul class="areas-container">
+            <li class="area">
+                <?php
+                    $area1 = get_field('area_1');
+                    echo "<pre>";
+                        var_dump($area1);
+                    echo "</pre>;"
+                ?>
+            </li>
+        </ul>
+    </section>
+
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
